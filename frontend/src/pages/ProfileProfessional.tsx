1679066e9bf2f70cc7628ffce7ebd1546400618c
Luis Henrique <luiss18.code@gmail.com>
2025-06-05 16:09:39 -0300

    feat: adicionar funcionalidade de favoritos na página de perfil profissional

--- frontend/src/pages/ProfileProfessional.tsx
+++ frontend/src/pages/ProfileProfessional.tsx
@@ -13,7 +13,6 @@ import toast, { Toaster } from 'react-hot-toast'
 import { getCookie } from '@/utlis/cookies'
 import { PostResponse } from '@/types/postTypes'
 import { addFavorite } from '@/api/favApi'
-import { PostResponse } from '@/types/postTypes'
 
 export const ProfileProfessional = () => {
   const [rating, setRating] = useState(0)
